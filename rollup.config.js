1900843bbc5f1a7c95923b764a95bab7f452d572
Mike Smullin <mike@smullindesign.com>
2018-07-14 16:58:53 -0600

    remove unused code

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import uglify from 'rollup-plugin-uglify-es';
 import gzip from 'rollup-plugin-gzip';
-import copy from 'rollup-plugin-copy';
 
 export default [
 	{
@@ -9,14 +8,6 @@ export default [
 			file: 'dist/m-bundle.js',
 			format: 'iife'
 		},
-		// plugins: [
-		// 	copy({
-		// 		'node_modules/highlight.js/lib/highlight.js': 'docs/highlight.js/highlight.js',
-		// 		'node_modules/highlight.js/lib/languages/javascript.js': 'docs/highlight.js/languages/javascript.js',
-		// 		'node_modules/highlight.js/styles/atom-one-dark.css': 'docs/highlight.js/styles/atom-one-dark.css', // or github is nice
-		// 		verbose: true
-		// 	}),
-		// ]
 	},
 	{
 		input: 'src/bundle.js',

3c7d5e20f25513c9c1f2bda6362f2c1c941025a9
Mike Smullin <mike@smullindesign.com>
2018-08-03 00:30:59 -0600

    markdown optimizations and support for blockquotes

--- src/extras/markdown-jxml.js
+++ src/extras/markdown-jxml.js
@@ -33,35 +33,36 @@ const W = (cond,k,o) => cond ? { [k]: o } : o, // wrap
  *   Primarily used internally by recursion.
  * @return {Object} - JXML.
  */
-export const markdown = (str, integrate=o=>o, inlineOnly=false) => {
+const markdown = (str, integrate=o=>o, inlineOnly=false) => {
 	// multi-pass tokenizer
-	// pass 1: chunks (becomes p tag if any leftover text)
+	// pass 1: inline-block elements (code, paragraph delim)
 	let chunks = t([str], NA,
-		/^~~~(\w{1,99})?(?:\r?\n|$)([\s\S]{1,9999}?)(?:\r?\n|$)~~~(?:\r?\n|$)/gm, m =>
+		/^~~~(\w{1,99})?(?:\r?\n)([\s\S]{1,9999}?)(?:\r?\n)~~~$|((?:\r?\n){2})/gm, m =>
 		is(m[2]) ? [ 'C', m[1], m[2] ] : // [ C = code block, 1 lang, 2 text ]
+		is(m[3]) ? [ 'S' ] : // S = double CR LF, paragraph delimiter
 		NA, I);
 
-	// pass 2: block elements (header, list item, otherwise p)
+	// pass 2: block elements (header, list item, blockquote)
 	if (!inlineOnly)
 		t(chunks, NA,
-			/(?:^(#{1,6})[ \t]{0,99}(.{1,999})(?:\r?\n|$)|^([ ]{0,99})([-*]|\d{1,3}\.)[ \t]{1,99}((?:.{1,9999}(?:\r?\n|$)(?!\1[-*]|\1\d{1,3}\.)){1,9999})(?:\r?\n|$))/gm, m =>
+			/^(#{1,6})[ \t]{0,99}(.{1,999})(?:\r?\n|$)|^([ ]{0,99})([-*]|\d{1,3}\.)[ \t]{1,99}((?:.{1,999}(?:\r?\n|$)(?!\1[-*]|\1\d{1,3}\.)){1,999})(?:\r?\n|$)|^>[ \t]{1,99}(.{1,999})(?:\r?\n|$)/gm, m =>
 			is(m[2]) ? [ 'H', m[1].length, m[2] ] : // H = heading, 1 lvl, 2 text
 			is(m[5]) ? [ 'L', m[4], // L = list item, 1 style, 2 text
 				m[5].replace(new RegExp('^'+m[3],'gm'), '') ] :
+			is(m[6]) ? [ 'B', m[6] ] : // [ B = blockquote, 1 text ]
 			NA, I);
 
-	// pass 3: inline/atomic elements (br, link, em)
+	// pass 3: inline/atomic elements (br, link, em, otherwise P)
 	t(chunks, NA,
-		/(?:(  $)|\[(.{1,999}?)\]\(((?:\w{1,99}:)?\/?\/?[-A-Za-z0-9+&@#/%?=~_()|!:,.;]{0,2083}[-A-Za-z0-9+&@#/%=~_()|])\)|([*_~]{1,2})([\s\S]{1,999}?)\4|((?:\r?\n){2}))/gm, m =>
-		is(m[1]) ? [ 'R' ] : // R = line-bReak
+		/(  (?:\r?\n|$))|\[(.{1,999}?)\]\(((?:\w{1,99}:)?\/?\/?[-A-Za-z0-9+&@#/%?=~_()|!:,.;]{0,2083}[-A-Za-z0-9+&@#/%=~_()|])\)|([*_~]{1,2})([\s\S]{1,999}?)\4/gm, m =>
+		is(m[1]) ? [ 'R' ] : // R = 2-space line-bReak
 		is(m[2]) ? [ 'A', m[3], m[2] ] : // A = hyperlink, 1 href, 2 anchor
 		is(m[5]) ? [ 'E', m[4], m[5] ] : // E = emphasis, 1 type, 2 text
-		is(m[6]) ? [ 'S' ] : // double CR LF is paragraph delimiter
-		NA, t=>''===t.trim() ? NA : [ 'P', t ]);
+		NA, t=>[ 'T', t ]); // T = text node
 
 	// pass 4: lexer + compiler (to JXML)
 	return S(t(chunks, chunks.map(c=>c[0]).join(''),
-		/(?:(S)|(H)|(L{1,999})|(C)|([PEAR]{1,999}))/g, m => {
+		/(S)|(H)|(L{1,999})|(C)|(B{1,99})|([EATR]{1,999})/g, m => {
 		const i = m.index,
 			F = cb => // single or flat map
 				S(chunks.slice(i, i + m[0].length)
@@ -79,16 +80,21 @@ export const markdown = (str, integrate=o=>o, inlineOnly=false) => {
 				$class: chunks[i][1],
 				_: chunks[i][2] }}}) :
 
-			is(m[5]) ? integrate(W(!inlineOnly, 'p', F(m =>
-				'P'===m[0] ? m[1] : // paragraph
+			is(m[5]) ? { blockquote:
+				markdown(F(m=>m[1]).join('\n'), integrate) } :
+
+			is(m[6]) ? integrate(W(!inlineOnly, 'p', F(m =>
 				'E'===m[0] ? { [ // emphasis
 					/^[*_]$/.test(m[1]) ? 'strong' : // bold
 					'~' === m[1] ? 'code' : // monospace
 					'~~' === m[1] ? 's' : // strikethrough
 					'em']: markdown(m[2], integrate, true) } : // italics
 				'A'===m[0] ? integrate({ a: { $href: m[1], _: m[2] }}) : // hyperlink
+				'T'===m[0] ? m[1] : // paragraph
 				'R'===m[0] ? { br: {} } : // line-break
 				NA))) :
 			NA;
 		}));
 };
+
+export default markdown;
--- test/extras/markdown-jxml.js
+++ test/extras/markdown-jxml.js
@@ -1,6 +1,6 @@
 const assert = require('chai').assert;
 require('babel-register');
-const { markdown } = require('../../src/extras/markdown-jxml');
+const markdown = require('../../src/extras/markdown-jxml').default;
 
 const SAMPLE_INPUT =
 `
@@ -25,7 +25,11 @@ in it
 
 this has a [link](http://www.google.com/)
 
-this has _emphasis_ *of* **various** __kinds__ ~inline code~
+this has emphasis of various kinds *bold* __italics__ ~~strike~~ ~mono~
+
+> # blockquote
+> can have  
+> normal formatting
 
 # h1
 ## h2
@@ -34,11 +38,11 @@ this has _emphasis_ *of* **various** __kinds__ ~inline code~
 ##### h5
 ###### h6
 
-- ordered
+- ordered  
   multiline
 - list
 
-1. numbered
+1. numbered  
    multiline
 2. list
 
@@ -61,124 +65,162 @@ describe('markdown-jxml.js', () => {
 
 	it('markdown', () => {
 		const output = markdown(SAMPLE_INPUT);
+		// console.log(JSON.stringify(output, null, '\t'));
 		assert.deepEqual(output, [
-            {
-                "pre": {
-                    "code": {
-                        "$class": "js",
-                        "_": "if (true) {\n\talert('hello world');\n}"
-                    }
-                }
-            },
-            {
-                "pre": {
-                    "code": {
-                    	"$class": undefined,
-                        "_": "random\n   code"
-                    }
-                }
-            },
-            {
-                "p": "\nthis would be a paragraph\nmade of multiple lines\nbut which turns into just one line."
-            },
-            {
-                "p": [
-                    "this would be a paragraph",
-                    {
-                        "br": {}
-                    },
-                    "\nwith line breaks",
-                    {
-                        "br": {}
-                    },
-                    "\nin it"
-                ]
-            },
-            {
-                "p": [
-                    "this has a ",
-                    {
-                        "a": {
-                            "$href": "http://www.google.com/",
-                            "_": "link"
-                        }
-                    }
-                ]
-            },
-            {
-                "p": [
-                    "this has ",
-                    {
-                        "strong": "emphasis"
-                    },
-                    {
-                        "strong": "of"
-                    },
-                    {
-                        "em": "various"
-                    },
-                    {
-                        "em": "kinds"
-                    },
-                    {
-                        "code": "inline code"
-                    }
-                ]
-            },
-            {
-                "h1": {
-                    "_": "h1"
-                }
-            },
-            {
-                "h2": {
-                    "_": "h2"
-                }
-            },
-            {
-                "h3": {
-                    "_": "h3"
-                }
-            },
-            {
-                "h4": {
-                    "_": "h4"
-                }
-            },
-            {
-                "h5": {
-                    "_": "h5"
-                }
-            },
-            {
-                "h6": {
-                    "_": "h6"
-                }
-            },
-            {
-                "ul": [
-                    {
-                        "li": {
-                            "p": "ordered\n  multiline"
-                        }
-                    },
-                    {
-                        "li": {
-                            "p": "list\n"
-                        }
-                    },
-                    {
-                        "li": {
-                            "p": "numbered\n   multiline"
-                        }
-                    },
-                    {
-                        "li": {
-                            "p": "list\n"
-                        }
-                    }
-                ]
-            }
-        ]);
+			{
+				"p": "\n"
+			},
+			{
+				"pre": {
+					"code": {
+						"$class": "js",
+						"_": "if (true) {\n\talert('hello world');\n}"
+					}
+				}
+			},
+			{
+				"pre": {
+					"code": {
+						"$class": undefined,
+						"_": "random\n   code"
+					}
+				}
+			},
+			{
+				"p": "this would be a paragraph\nmade of multiple lines\nbut which turns into just one line."
+			},
+			{
+				"p": [
+					"this would be a paragraph",
+					{
+						"br": {}
+					},
+					"with line breaks",
+					{
+						"br": {}
+					},
+					"in it"
+				]
+			},
+			{
+				"p": [
+					"this has a ",
+					{
+						"a": {
+							"$href": "http://www.google.com/",
+							"_": "link"
+						}
+					}
+				]
+			},
+			{
+				"p": [
+					"this has emphasis of various kinds ",
+					{
+						"strong": "bold"
+					},
+					" ",
+					{
+						"em": "italics"
+					},
+					" ",
+					{
+						"s": "strike"
+					},
+					" ",
+					{
+						"code": "mono"
+					},
+				]
+			},
+			{
+				"blockquote": [
+					{
+						"h1": {
+							"_": "blockquote"
+						}
+					},
+					{
+						"p": [
+							"can have",
+							{
+								"br": {}
+							},
+							"normal formatting"
+						]
+					}
+				]
+			},
+			{
+				"h1": {
+					"_": "h1"
+				}
+			},
+			{
+				"h2": {
+					"_": "h2"
+				}
+			},
+			{
+				"h3": {
+					"_": "h3"
+				}
+			},
+			{
+				"h4": {
+					"_": "h4"
+				}
+			},
+			{
+				"h5": {
+					"_": "h5"
+				}
+			},
+			{
+				"h6": {
+					"_": "h6"
+				}
+			},
+			{
+				"ul": [
+					{
+						"li": {
+							"p": [
+								"ordered",
+								{
+									"br": {}
+								},
+								"  multiline"
+							]
+						}
+					},
+					{
+						"li": {
+							"p": "list"
+						}
+					}
+				]
+			},
+			{
+				"ol": [
+					{
+						"li": {
+							"p": [
+								"numbered",
+								{
+									"br": {}
+								},
+								"   multiline"
+							]
+						}
+					},
+					{
+						"li": {
+							"p": "list"
+						}
+					}
+				]
+			}
+		]);
 	});
 });

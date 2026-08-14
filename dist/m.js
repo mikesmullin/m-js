6840209e6ae554d249a604a02b2e7adf9c5039f9
me <user@home.local>
2026-08-14 08:23:42 -0600

    Publish m.js v3.3.0 (dist + docs)

--- dist/m.js
+++ dist/m.js
@@ -1,4 +1,4 @@
-/*! m.js v3.2.2 | MIT | https://mikesmullin.github.io/m-js/ */
+/*! m.js v3.3.0 | MIT | https://mikesmullin.github.io/m-js/ */
 
 // src/router.js
 var RX_ABSOLUTE_URL = /^(?:\w{1,99}:)?\/\//;
@@ -2250,7 +2250,7 @@ function attachLifecycle(vnode, ast, scope, ctx) {
 }
 
 // src/m.js
-var VERSION = "3.2.2";
+var VERSION = "3.3.0";
 var rootEl = null;
 var rootFactory = null;
 var rootInstance = null;
